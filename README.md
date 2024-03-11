7cd0e749c17a0c679cfa0d2d2fa89c63ae0a9d64
Rohit Balage <thatcomputerboy26@gmail.com>
2024-03-11 18:54:37 -0400

    added README

---
+++ README.md
@@ -0,0 +1,5 @@
+### Simple regression model | Android + ML | kotlin
+
+#
+
+this app is a simple demo for regression model in android
--- app/src/main/java/com/example/rrbofficial/MainActivity.kt
+++ app/src/main/java/com/example/rrbofficial/MainActivity.kt
@@ -6,6 +6,10 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.tensorflow.lite.Interpreter
+import java.io.FileInputStream
+import java.io.IOException
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
 
 var interpreter:Interpreter? = null
 class MainActivity : AppCompatActivity() {
@@ -30,15 +34,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
     
-//    @Throws(IOException::class)
-//    private fun loadModelFile(): MappedByteBuffer? {
-//        val assetFileDescriptor = this.assets.openFd("linear.tflite")
-//        val fileInputStream =
-//            FileInputStream(assetFileDescriptor.fileDescriptor)
-//        val fileChannel = fileInputStream.channel
-//        val startOffset = assetFileDescriptor.startOffset
-//        val length = assetFileDescriptor.length
-//        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, length)
-//    }
+    @Throws(IOException::class)
+    private fun loadModelFile(): MappedByteBuffer? {
+        val assetFileDescriptor = this.assets.openFd("linear.tflite")
+        val fileInputStream =
+            FileInputStream(assetFileDescriptor.fileDescriptor)
+        val fileChannel = fileInputStream.channel
+        val startOffset = assetFileDescriptor.startOffset
+        val length = assetFileDescriptor.length
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, length)
+    }
 
 }
